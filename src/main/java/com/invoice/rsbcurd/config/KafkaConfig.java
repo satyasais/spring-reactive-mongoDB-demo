a54bdaac2b5a4669957e24ff5a6afda52fd39e9f
Satyasai Sribhashyam <67517758+satyasais@users.noreply.github.com>
2023-01-04 19:01:47 +0530

    KafkaConfigure

--- src/main/java/com/invoice/rsbcurd/config/KafkaConfig.java
+++ src/main/java/com/invoice/rsbcurd/config/KafkaConfig.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -26,18 +28,18 @@ public class KafkaConfig {
     public ProducerFactory<String, String> producerFactory()
     {
   
-        Map<String, Object> config = new HashMap<>();
-  
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+		Map<String, Object> config = new HashMap<>();
+		  
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
                    "127.0.0.1:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG,
                    "group_id");
         config.put(
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-            StringDeserializer.class);
+        		ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+            StringSerializer.class);
         config.put(
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-            StringDeserializer.class);
+        		ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        		StringSerializer.class);
   
         return new DefaultKafkaProducerFactory<>(config);
     }
